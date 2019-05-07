fc75964d52e97ddb08b9200c88142857fd49509e
Quinten van den Herik <99041030@mydavinci.nl>
2019-05-07 09:56:58 +0200

    Added Shoppingcart package, modiffied some blade files

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -5,6 +5,6 @@ use Faker\Generator as Faker;
 $factory->define(App\Product::class, function (Faker $faker) {
     return [
         'name' => $faker->word,
-        'price' => $faker->randomFloat($nbMaxDecimals = 2, $min = 1, $max = 50)
+        'price' => $faker->randomFloat($nbMaxDecimals = 2, $min = 1, $max = 20),
     ];
 });
--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -6,13 +6,14 @@
         <h1>Products</h1>
         <div class="products">
             @foreach ($products as $product)
-                <div class="product"style="margin: auto;
-  width: 200px; height: 200px; text-align: center; border: black; border-style: solid; display: inline-block; margin: 10px;">
-                    <p>{{ $product->name }}</p>
-                    <img src="image/ImageComingSoon.png{{-- $product->name }}.jpg--}}" style="width: 125px; height: 125px;"></img>
-                    <p>{{ $product->price }}</p>
-                </div>
+                <a href="#" class="product" style="margin: auto;
+  width: 200px; height: 200px; text-align: center; border: black; border-style: solid; display: inline-block; margin: 10px; text-decoration: none; color: black;">
+                    <p>{{ $product->name }} background</p>
+                    <img src="https://picsum.photos/id/{{ $product->id }}/194/125" style="width: 194px; height: 125px;"></img>
+                    <p>€{{ $product->price }}</p>
+                </a>
             @endforeach
         </div>
+        <div style="display: -webkit-inline-box;">{{ $products->links() }}</div>
     </div>
 @endsection
